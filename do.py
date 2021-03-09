9c73418cd695a2f5bc0e76b08ddc8b47f442f28a
徐少文 <15011128926@163.com>
2021-03-09 11:12:47 +0800

    using files to input data

--- do.py
+++ do.py
@@ -4,99 +4,63 @@ import xlrd
 #2.0.1版本的xlrd不能操作xlsx文件，将xlrd回退至1.2.0
 import xlwt
 
-titles=['序号','公司名称','问卷填写人','办公电话','手机','电子邮箱',
-'责任人','任职部门','职务','联系方式',
-'证券从业资格（人）','基金从业资格（人）','律师从业资格（人）','注册会计师（人）','特许金融分析师（人）','金融风险管理师（人）',
-'海外留学经历（人）','海外工作经历（人）','海外留学&工作（人）',
-'交易所培训（人天）','公司内部培训（人天）','其他培训（人天）','培训总频次（人天）','费用总支出',
-'校园招聘（人）','社会招聘-期货公司（人）','社会招聘-基金（人）','社会招聘-金融机构（人）','社会招聘-现货行业（人）','社会招聘-其他（人）','社会招聘（人）',
-'离职人员（人）','离职-期货（人）','离职-基金（人）','离职-金融（人）','离职-现货（人）','离职-其他（人）','离职原因-首要','离职原因-次要','离职原因-第三','离职原因-其他',
-'资产管理子公司','人员数量','较2018年末增加','风险管理子公司','人员数量','较2018年年末增加','子公司人员数量变化原因','期货行业吸引力得分','意见建议','期货从业人员管理得分','意见建议']
+outputname='2020年度期货从业人员状况调查汇总表.xls'
+if os.path.exists(outputname):
+    os.remove(outputname)
 
-def add_content(row,column,l,table,flag=False):
-    s=table.cell_value(row,column)
-    if not flag:
-        l.append(s)
-    else:
-        try:
-            l.append(int(s))
-        except ValueError:
-            l.append(0)
-
+#设置输出文件
 workbook = xlwt.Workbook(encoding='utf-8')
 worksheet=workbook.add_sheet('Sheet1')
 
-colums=0
-for labels in titles:#输出表头数据
-    worksheet.write(0,colums,label=labels)
-    colums+=1
+#打开配置文件
+with open('titles.txt','r') as f:
+    l=f.readlines()
 
+#输出表头数据
+worksheet.write(0,0,label='序号')
+columns=1
+for name in l:
+    labels=name.split()[0]
+    worksheet.write(0,columns,label=labels)
+    columns+=1
+
+#设置源excel文件
 filepath='.\\'
 filelist = os.listdir(filepath)
+
 rows=1
-for xls in filelist:#遍历目录下所有文件
+#遍历目录下所有文件
+for xls in filelist:
     if os.path.splitext(xls)[-1]=='.xlsx' or os.path.splitext(xls)[-1]=='.xls':#过滤文件名
         try:
             data = xlrd.open_workbook(filepath+xls)
         except PermissionError:
             continue
+        
         else:
+            #序号
             worksheet.write(rows,0,label=rows)
             table = data.sheets()[0]
+
+            columns=1
             
-            content=[]
+            for items in l:
+                x=int(items.split()[1])-1
+                y=ord(items.split()[2])-ord('a')
 
-            #期货公司基本信息
-            add_content(3,2,content,table)
-            add_content(5,1,content,table)
-            add_content(5,3,content,table,True)
-            add_content(5,5,content,table,True)
-            add_content(5,7,content,table)
-            add_content(6,1,content,table)
-            add_content(6,3,content,table)
-            add_content(6,5,content,table)
-            add_content(6,7,content,table,True)
+                is_int=items.split()[-1]=='i'
 
-            #期货从业人员结构信息
-            ##专业化人才储备情况
-            for p in range(9,15):
-                add_content(p,7,content,table,True)
+                content=table.cell_value(x,y)
 
-            ##国际化人才储备情况
-            for p in range(16,19):
-                add_content(p,7,content,table,True)
+                if is_int:
+                    try:
+                        content=int(content)
+                    except ValueError:
+                        content=0
 
-            #就业培训情况
-            for p in range(20,25):
-                add_content(p,7,content,table)
-            
-            #从业人员流动情况
-            for p in range(26,39):
-                add_content(p,7,content,table,True)
-
-            for p in range(39,43):
-                add_content(p,6,content,table)
-
-            #期货公司子公司情况
-            add_content(44,1,content,table)
-            add_content(44,4,content,table,flag=True)
-            add_content(44,7,content,table,flag=True)
-            add_content(45,1,content,table)
-            add_content(45,4,content,table,flag=True)
-            add_content(45,7,content,table,flag=True)
-            add_content(46,1,content,table)
-
-            #人才队伍建设意见建议
-            add_content(48,7,content,table,flag=True)
-            add_content(49,0,content,table)
-            add_content(50,7,content,table,flag=True)
-            add_content(51,0,content,table)
-
-            col=1
-            for con in content:
-                worksheet.write(rows,col,label=con)
-                col+=1
+                worksheet.write(rows,columns,label=content)
+                columns += 1
 
             rows+=1
-        
-workbook.save('2020年度期货从业人员状况调查汇总表.xls')
+
+workbook.save(outputname)
---
+++ readme.md
@@ -0,0 +1,21 @@
+一个非常好用的excel合并脚本
+
+# 场景
+你现在收集上了很多很多调查问卷，令人遗憾的是，这些每份问卷都保存在一个excel文件中。现在你的上司想让你将这些问卷合并到一个excel中，你非常苦恼。但是有了python的辅助，这些都不是问题。按照此框架模板开发属于自己的程序，只需要几分钟就可以完成别人数小时的工作，从此告别加班。
+
+# Requirement
+参见文件requirements.txt
+
+## 有关于xlrd
+需要注意，xlrd在1.2.0版本后不再支持.xlsx文件，如果已经安装了高级版本并且想处理.xlsx文件需要首先卸载后再安装。方法如下：
+```shell
+pip3 uninstall xlrd
+pip3 install xlrd==1.2.0
+```
+
+# 使用
+```shell
+cd project
+pip3 install requirements
+python do.py
+```
